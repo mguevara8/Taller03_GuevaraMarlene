c0b1c5e300420c9d0820d02fb5f455317a28e75d
Kevin Escorcia <Kescorci5@gmail.com>
2024-09-24 13:04:19 -0500

    m1

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css'
+import miLogo from "../src/assets/img/miLogo.jpg"
+
+function App() {
+
+  return (
+    <>
+    <div>
+      <img src={miLogo} alt="Logo React" />
+      <h1>¡¡Hola Marle</h1>
+      <p>Este es mi primer proyecto con React </p>
+     </div>
+    </>
+  )
+}
+
+export default App
